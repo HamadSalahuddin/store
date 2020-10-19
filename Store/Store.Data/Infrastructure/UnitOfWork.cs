7fee14ad5ff85404e81266858717cbd2650120f4
Hamad Salahuddin <hamadsalahuddin@gmail.com>
2020-10-19 15:35:34 +0500

    Added DB Context class. Added Base Repository and Entity Specific Repository. Added GitIgnore

---
+++ Store/Store.Data/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Data.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly IDbFactory dbFactory;
+        private StoreEntities dbContext;
+
+        public UnitOfWork(IDbFactory dbFactory)
+        {
+            this.dbFactory = dbFactory;
+        }
+
+        public StoreEntities DbContext
+        {
+            get { return dbContext ?? (dbContext = this.dbFactory.Init()); }
+        }
+
+        public void Commit()
+        {
+            dbContext.Commit();
+        }
+    }
+}
---
+++ Store/Store.Data/Repositoires/CategoryRepository.cs
@@ -0,0 +1,33 @@
+﻿using Store.Data.Infrastructure;
+using Store.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Data.Repositoires
+{
+    public class CategoryRepository: RepositoryBase<Category>, ICategoryRepository
+    {
+        public CategoryRepository(IDbFactory dbFactory)
+            : base(dbFactory) { }
+
+        public Category GetCategoryByName(string categoryName)
+        {
+            return this.DbContext.Categories
+                .FirstOrDefault(category => category.Name.ToLower() == categoryName.ToLower());
+        }
+
+        public override void Update(Category entity)
+        {
+            entity.DateUpdated = DateTime.Now;
+            base.Update(entity);
+        }
+    }
+
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        Category GetCategoryByName(string categoryName);
+    }
+}
