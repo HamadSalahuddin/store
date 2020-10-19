a8031bfa2bdd2009f9131f044c3dde41480407cf
Hamad Salahuddin <hamadsalahuddin@gmail.com>
2020-10-19 11:59:11 +0500

    Added EntityFramework Package Added ModelConfiguration classes. Added DBContext class. Added StoreSeedData to see the data into DB class.

---
+++ Store/Store.Data/Configuration/GadgetConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Store.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Data.Configuration
+{
+    public class GadgetConfiguration: EntityTypeConfiguration<Gadget>
+    {
+        public GadgetConfiguration()
+        {
+            ToTable("Gadgets");
+            Property(gadget => gadget.Name).IsRequired().HasMaxLength(50);
+            Property(gadget => gadget.Price).IsRequired().HasPrecision(8, 2);
+            Property(gadget => gadget.CategoryId).IsRequired();
+        }
+    }
+}
